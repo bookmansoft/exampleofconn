00e4e397ff41ac64a028968a0eeafa1ed21c20a7
bookmansoft <bianque@qq.com>
2022-12-24 15:02:29 +0800

    4.2.1

--- test/ec.js
+++ test/ec.js
@@ -12,19 +12,14 @@ const VALLNET = require('../lib/index');
 const remote = new VALLNET(config);         //创建RPC实例
 
 let env = {
-    message: '风吹草低现牛羊', 
-    cpa: {name: "cp-"+ uuid().slice(0,33)},
-    cpb: {name: "cp-"+ uuid().slice(0,33)},
     alice: {name: "cp-"+ uuid().slice(0,33), erid:[]},
     bob: {name: "cp-"+ uuid().slice(0,33), erid:[]},
-    content: () => utils.randomHash(),
+    //电子合同对象
+    agreement: {
+        title: 'agreement',
+        body: 'hello world',
+    }
 };
-
-//电子合同对象
-let agreement = {
-    title: 'agreement',
-    body: 'hello world',
-}
 
 describe('GIP0024 电子签章', function() {
     it('Alice生成地址', async () => {
@@ -53,12 +48,30 @@ describe('GIP0024 电子签章', function() {
     it('个人签发: Bob签发证书', async () => {
         let ret = await remote.execute('ca.issue.public', [
             {
-                hash: env.content(),    //实体证书内容哈希(64字节HEX字符串)
-                height: 0,              //有效期，填0表示使用默认值
+                hash: utils.randomHash(),   //实体证书内容哈希(64字节HEX字符串)
+                height: 0,                  //有效期，填0表示使用默认值
             },
-            env.bob.address,            //见证地址, 当前钱包必须拥有该地址的控制权
+            env.bob.address,                //见证地址, 当前钱包必须拥有该地址的控制权
         ]);
         assert(ret.erid);
+        /**
+        {
+            oper: 'erIssue',
+            erid: 'a5ea2519a44cf11dedfb8c886eded86f5d69d780e3593920d6d6679dd1169c1f',
+            witness: '0385e91443559236cc5dfc59dba6f3b06b84f5862b907d836002d70705a80a797f',
+            validHeight: 2752,
+            startHeight: 0,
+            signature: '3044022071828ebf4815ebf7da27a84028aade06aa1dde1cbb85755aeab887bc7a5c323302202a864597be475bcb628f0f8059f417d882ff83a7a2c218b7583d32baa6bb382b',
+            source: {
+                subjectName: 'a5ea2519a44cf11dedfb8c886eded86f5d69d780e3593920d6d6679dd1169c1f',
+                pubkey: '',
+                subjectHash: 'a5ea2519a44cf11dedfb8c886eded86f5d69d780e3593920d6d6679dd1169c1f',
+                cluster: '',
+                cid: '',
+                uid: 0
+            }
+        }
+         */
         env.alice.erid.unshift(ret.erid); //缓存erid
 
         await remote.execute('miner.generate.admin', [1]);
@@ -112,7 +125,7 @@ describe('GIP0024 电子签章', function() {
 
     it('签署电子合同: Alice和Bob签署电子合同', async () => {
         //Alice本地签署合同
-        env.alice.agreement = verify.signData(agreement, {
+        env.alice.agreement = verify.signData(env.agreement, {
             network: 'testnet',
             alice_prv: env.alice.prikey,
             alice_pub: env.alice.pubkey,
@@ -174,75 +187,6 @@ describe('GIP0024 电子签章', function() {
         await verifyContract(env.alice.agreement, ret.list[0]);
     });
 
-    it('机构注册: CPA注册', async () => {
-        let ret = await remote.execute('cp.create', [env.cpa.name, '127.0.0.1']);
-        assert(!ret.error);
-        env.cpa.cid = ret.cid;
-        env.cpa.address = ret.addr;
-
-        await remote.execute('miner.generate.admin', [1]);
-        await remote.wait(500);
-    });
-
-    it('机构注册: CPB注册', async () => {
-        let ret = await remote.execute('cp.create', [env.cpb.name, '127.0.0.1']);
-        assert(!ret.error);
-        env.cpb.cid = ret.cid;
-        env.cpb.address = ret.addr;
-
-        await remote.execute('miner.generate.admin', [1]);
-        await remote.wait(500);
-    });
-
-    it('机构签发: CPA为Alice签发证书', async () => {
-        let ret = await remote.execute('ca.issue.public', [
-            {
-                hash: env.content(),     //内容哈希
-                height: 0,               //相对有效期
-            },
-            env.cpa.address,            //签发地址
-            env.alice.pubkey,           //address pubkey
-        ]);
-        assert(ret.erid);
-        env.alice.erid.unshift(ret.erid);
-
-        await remote.execute('miner.generate.admin', [1]);
-        await remote.wait(500);
-    });
-
-    it('查询证书：根据证书编号查询证书内容', async () => {
-        let erid = env.alice.erid[0];
-        let ret = await remote.execute('ca.list', [[['erid', erid]]]);
-        assert(ret.list[0].erid == erid);
-    });
-
-    it('验证证书：验证证书的有效性', async () => {
-        let erid = env.alice.erid[0];
-        let ret = await remote.execute('ca.verify', [erid]);
-        assert(ret && ret.verify);
-        //console.log(ret);
-    });
-
-    it('查询信用: 查询CA信用等级', async () => {
-        let ret = await remote.execute('ca.rank', [env.cpa.cid]);
-        assert(!ret);
-    });
-
-    it('机构增信: CPB为CPA增信', async () => {
-        let ret = await remote.execute('ca.enchance', [
-            env.cpa.cid,
-            env.cpb.cid,
-        ]);
-        assert(!ret.error);
-        //确保数据上链
-        await remote.execute('miner.generate.admin', [1]);
-    });
-
-    it('查询信用: 查询CA信用等级', async () => {
-        let ret = await remote.execute('ca.rank', [env.cpa.cid]);
-        assert(!!ret && ret > 0);
-    });
-
     before(async ()=>{
         await remote.execute('miner.setsync.admin', [true]);
         let ret = await remote.execute('block.tips', []);
@@ -252,21 +196,25 @@ describe('GIP0024 电子签章', function() {
     });
 });
 
-async function verifyContract(agreement, result) {
+/**
+ * 电子合同第三方验证流程
+ * @param {*} signed    带Alice签名的电子合同文本
+ * @param {*} result    主网公布的电子合同凭证
+ */
+async function verifyContract(signed, result) {
     assert(!!result);
 
     //验证本地合同是自洽的(经过了Alice的签名)
-    assert.strictEqual(true, verify.verifyData(agreement));
+    assert.strictEqual(true, verify.verifyData(signed));
 
     //验证本地合同中的公钥和链上合同中的公钥一致(合同哈希值和签名公钥已经上链存证)
-    assert.strictEqual(result.source.pubkey, agreement.data.pubkey);
+    assert.strictEqual(result.source.pubkey, signed.data.pubkey);
 
     //验证本地合同中的哈希值和链上合同中的哈希值一致
-    assert.strictEqual(result.source.subjectHash, utils.sha256(JSON.stringify(agreement.data)).toString('hex'));
+    assert.strictEqual(result.source.subjectHash, utils.sha256(JSON.stringify(signed.data)).toString('hex'));
     
     //验证链上合同是自洽的(经过了Bob的见证)
     let ret = await remote.execute('ca.verify', [result.erid]);
     assert(ret && ret.verify);
     assert(ret.witness == env.bob.pubkey);
 }
-
